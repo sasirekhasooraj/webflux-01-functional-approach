655d4dd89a67fc4a024b8e450db3f3acdcf3a16e
ssasirekha <ssasirekha@gmail.com>
2021-10-26 10:15:09 +0530

    Initial commit

---
+++ src/main/java/c/jbd/webflux/HelloHandler.java
@@ -0,0 +1,42 @@
+package c.jbd.webflux;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+@Component
+public class HelloHandler {
+  /**
+   * Serves a plain_text
+   *
+   * @param request
+   * @return
+   */
+  public Mono<ServerResponse> monoMessage(ServerRequest request) {
+    return ServerResponse.ok()
+      .contentType(MediaType.TEXT_PLAIN)
+      .body(
+        Mono.just("Welcome to RP programming"), String.class
+      );
+  }
+
+  /**
+   * Serves a JSON stream
+   *
+   * @param request
+   * @return
+   */
+  public Mono<ServerResponse> fluxMessage(ServerRequest request) {
+    return ServerResponse.ok()
+      .contentType(MediaType.APPLICATION_STREAM_JSON)
+      .body(
+        Flux.just("Welcome ", "to ", "RP Programming")
+          .delayElements(Duration.ofSeconds(1)).log(), String.class
+      );
+  }
+}
